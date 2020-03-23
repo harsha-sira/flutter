322b847ef7403b5d83bfccb62b12416f69f62838
harsha <harshasandamal100@gmail.com>
2020-03-23 09:04:37 +0530

    flutter projects initial commit

---
+++ flutter_helloworld/lib/screens/destination.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Destination extends StatefulWidget {
+  @override
+  _DestinationState createState() => _DestinationState();
+}
+
+class _DestinationState extends State<Destination> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Top Destinations',
+                style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.5),
+              ),
+              GestureDetector(
+                onTap: () => print("see all tapped"),
+                child: Text(
+                  'See All',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).primaryColor),
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ flutter_helloworld/lib/screens/home_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_helloworld/screens/destination.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _iconSelectedIndex = 0;
+  List<IconData> icons = [
+    FontAwesomeIcons.plane,
+    FontAwesomeIcons.bed,
+    FontAwesomeIcons.parachuteBox,
+    FontAwesomeIcons.biking,
+  ];
+
+  Widget _buildIcon(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _iconSelectedIndex = index;
+        });
+      },
+      child: Container(
+          height: 60.0,
+          width: 60.0,
+          decoration: BoxDecoration(
+              color: _iconSelectedIndex == index
+                  ? Theme.of(context).accentColor
+                  : Color(0xFFE7EBEE),
+              borderRadius: BorderRadius.circular(30.0)),
+          child: Icon(
+            icons[index],
+            size: 25.0,
+            color: Theme.of(context).primaryColor,
+          )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: ListView(
+      padding: EdgeInsets.symmetric(vertical: 30.0),
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 80.0),
+          child: Text(
+            'What would you like to find',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: icons
+              .asMap()
+              .entries
+              .map(
+                (MapEntry map) => _buildIcon(map.key),
+              )
+              .toList(),
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Destination()
+      ],
+    )));
+  }
+}
