abb01ce7dc975f784811a3095234ff190fe4fdc6
harsha <harshasandamal100@gmail.com>
2020-04-25 22:06:18 +0530

    create new project with android java support instead of kotlin

---
+++ flutter_learn_android_in_java/lib/screens/photoList.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_learn_android_in_java/model/Photo.dart';
+
+class PhotosList extends StatelessWidget {
+  final List<Photo> photos;
+
+  PhotosList({Key key, this.photos}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // debugPrint('test');
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: photos.length,
+      itemBuilder: (context, index) {
+        return Image.network(photos[index].thumbnailUrl);
+      },
+    );
+  }
+}
