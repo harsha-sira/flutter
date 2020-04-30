d87e5cffa0382b820486d59a6c95560e718b4d3b
harsha <harshasandamal100@gmail.com>
2020-04-30 23:39:20 +0530

    refactoring and adding platform channels

--- flutter_learn_network_calls/lib/screens/alarm_manager.dart
+++ flutter_learn_network_calls/lib/screens/alarm_manager.dart
@@ -5,7 +5,7 @@ import 'package:android_alarm_manager/android_alarm_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_learn_network_calls/screens/Call_back_operations.dart';
-import 'package:foreground_service/foreground_service.dart';
+// import 'package:foreground_service/foreground_service.dart';
 
 class AlarmManager extends StatefulWidget {
   AlarmManager({Key key}) : super(key: key);
@@ -38,7 +38,15 @@ class _AlarmManagerState extends State<AlarmManager> {
   static Future<void> callbackAlarm() async {
     print('Alarm fired!' + DateTime.now().toIso8601String());
     // CallBackService.run();
-    maybeStartFGS();
+    //check need for platform type
+    //This is for android
+    try {
+      await platform.invokeMethod(
+        'foregroundService',
+      );
+    } on PlatformException catch (e) {
+      print("Failed to get battery level: '${e.message}'.");
+    }
   }
 
   Future<void> initPlatformStateAlarm() async {
@@ -60,16 +68,7 @@ class _AlarmManagerState extends State<AlarmManager> {
     await AndroidAlarmManager.periodic(
         const Duration(minutes: 1), helloAlarmID, callbackAlarm,
         exact: true, wakeup: true, rescheduleOnReboot: true);
-    // try {
-    //   await platform.invokeMethod(
-    //     'runAlarmManager',
-    //     {
-    //       "duration": "minutes:1",
-    //     },
-    //   );
-    // } on PlatformException catch (e) {
-    //   print("Failed to get battery level: '${e.message}'.");
-    // }
+
     print("started alarm ...");
 
     setState(() {
@@ -85,47 +84,47 @@ class _AlarmManagerState extends State<AlarmManager> {
     });
   }
 
-  //use an async method so we can await
-  static void maybeStartFGS() async {
-    ///if the app was killed+relaunched, this function will be executed again
-    ///but if the foreground service stayed alive,
-    ///this does not need to be re-done
-    if (!(await ForegroundService.foregroundServiceIsStarted())) {
-      await ForegroundService.setServiceIntervalSeconds(5);
+  // //use an async method so we can await
+  // static void maybeStartFGS() async {
+  //   ///if the app was killed+relaunched, this function will be executed again
+  //   ///but if the foreground service stayed alive,
+  //   ///this does not need to be re-done
+  //   if (!(await ForegroundService.foregroundServiceIsStarted())) {
+  //     await ForegroundService.setServiceIntervalSeconds(5);
 
-      //necessity of editMode is dubious (see function comments)
-      await ForegroundService.notification.startEditMode();
+  //     //necessity of editMode is dubious (see function comments)
+  //     await ForegroundService.notification.startEditMode();
 
-      await ForegroundService.notification
-          .setTitle("Example Title: ${DateTime.now()}");
-      await ForegroundService.notification
-          .setText("Example Text: ${DateTime.now()}");
+  //     await ForegroundService.notification
+  //         .setTitle("Example Title: ${DateTime.now()}");
+  //     await ForegroundService.notification
+  //         .setText("Example Text: ${DateTime.now()}");
 
-      await ForegroundService.notification.finishEditMode();
+  //     await ForegroundService.notification.finishEditMode();
 
-      await ForegroundService.startForegroundService(foregroundServiceFunction);
-      await ForegroundService.getWakeLock();
-    }
+  //     await ForegroundService.startForegroundService(foregroundServiceFunction);
+  //     await ForegroundService.getWakeLock();
+  //   }
 
-    ///this exists solely in the main app/isolate,
-    ///so needs to be redone after every app kill+relaunch
-    await ForegroundService.setupIsolateCommunication((data) {
-      debugPrint("main received: $data");
-    });
-  }
+  //   ///this exists solely in the main app/isolate,
+  //   ///so needs to be redone after every app kill+relaunch
+  //   await ForegroundService.setupIsolateCommunication((data) {
+  //     debugPrint("main received: $data");
+  //   });
+  // }
 
-  static void foregroundServiceFunction() {
-    debugPrint("The current time is: ${DateTime.now()}");
-    ForegroundService.notification.setText("The time was: ${DateTime.now()}");
+  // static void foregroundServiceFunction() {
+  //   debugPrint("The current time is: ${DateTime.now()}");
+  //   ForegroundService.notification.setText("The time was: ${DateTime.now()}");
 
-    if (!ForegroundService.isIsolateCommunicationSetup) {
-      ForegroundService.setupIsolateCommunication((data) {
-        debugPrint("bg isolate received: $data");
-      });
-    }
+  //   if (!ForegroundService.isIsolateCommunicationSetup) {
+  //     ForegroundService.setupIsolateCommunication((data) {
+  //       debugPrint("bg isolate received: $data");
+  //     });
+  //   }
 
-    ForegroundService.sendToPort("message from bg isolate");
-  }
+  //   ForegroundService.sendToPort("message from bg isolate");
+  // }
 
   @override
   Widget build(BuildContext context) {
